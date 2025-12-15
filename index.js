c152a1256d221f4a9ec8c2d4306f0b0c68ca2840
Nawab-AS <59747708+Nawab-AS@users.noreply.github.com>
2025-12-15 00:49:06 -0500

    added project section

--- index.js
+++ index.js
@@ -18,7 +18,24 @@ const data = ref({
             description: "Description for project two.",
             image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDn24iCR7VE2le8OTW2jZ6pZW9NNMemSyeFQ&s",
             link: "https://example.com/project-two",
-            github: "https://github.com/example/project-two"
+            github: "https://github.com/example/project-two",
+            WIP: true
+        },
+        {
+            title: "Project three",
+            description: "Description for project three.",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDn24iCR7VE2le8OTW2jZ6pZW9NNMemSyeFQ&s",
+            link: "https://example.com/project-three",
+            github: "https://github.com/example/project-three",
+            WIP: true
+        },
+        {
+            title: "Project four",
+            description: "Description for project four.",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDn24iCR7VE2le8OTW2jZ6pZW9NNMemSyeFQ&s",
+            link: "https://example.com/project-four",
+            github: "https://github.com/example/project-four",
+            WIP: true
         }
     ],
 
