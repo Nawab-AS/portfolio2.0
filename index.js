d957871c4058c6f91c140fc6a69279e3d055d795
Nawab-AS <59747708+Nawab-AS@users.noreply.github.com>
2025-12-14 22:56:43 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const { createApp, ref } = Vue;
+
+const data = ref({
+    about: 'Hello, I am Hamid',
+
+    projects: [
+        {
+            title: "Project One",
+            description: "Description for project one.",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDn24iCR7VE2le8OTW2jZ6pZW9NNMemSyeFQ&s",
+            link: "https://example.com/project-one",
+            github: "https://github.com/example/project-one"
+        },
+        {
+            title: "Project two",
+            description: "Description for project two.",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDn24iCR7VE2le8OTW2jZ6pZW9NNMemSyeFQ&s",
+            link: "https://example.com/project-two",
+            github: "https://github.com/example/project-two"
+        }
+    ],
+
+    contact: {
+        email: "hello@example.com"
+    }
+});
+
+
+createApp({
+    setup() {
+        return {
+            data
+        }
+    }
+}).mount('body');
+
